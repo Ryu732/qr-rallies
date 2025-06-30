4af985358d6f706b7aa64155c2dd93c72009b866
Ryu732 <sakanaction001@gmail.com>
2025-07-01 03:10:44 +0900

    fix: スタンプの削除制約のタイプミス修正

--- models/rally.go
+++ models/rally.go
@@ -6,5 +6,5 @@ type Rally struct {
 	gorm.Model
 	RallyName string  `gorm:"not null;unique"`
 	Password  string  `gorm:"not null"`
-	Stamp     []Stamp `gorm:"constraint:Ondelete:CASCADE"`
+	Stamp     []Stamp `gorm:"constraint:OnDelete:CASCADE"`
 }
